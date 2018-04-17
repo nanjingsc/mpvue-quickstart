3311148755f39dee439ade4c043624731b5d1f12
Bobby-office <changxiaobo@corp-ci.com>
2018-04-17 15:41:27 +0800

    Vue use SCApi

--- template/src/main.js
+++ template/src/main.js
@@ -4,8 +4,10 @@
 {{/if_eq}}
 import Vue from 'vue'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+import SCApi from './utils/api'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+Vue.use(SCApi){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 App.mpType = 'app'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 const app = new Vue(App){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
